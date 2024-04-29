4e5a09978237f5e5eb44afa7e9aabe90def694b9
chocho77 <chavmom@gmail.com>
2024-04-29 08:11:23 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   src/Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 const Home = () => {
+    // let name = 'mario';
+   
+   const[name, setName] = useState('mario');  
+   const[age, setAge] = useState(25);
+
     const handleClick = (e) => {
         console.log('hello, ninjas', e);
     }
@@ -6,11 +13,23 @@ const Home = () => {
     const handleClickAgain = (name, e) => {
         console.log('hello ' + name, e.target);
     }
+
+    const handleClickName = () =>{
+        //name = 'luigi';
+        //console.log(name);
+        setName('luigi');
+        setAge(30);
+    }
     return ( 
         <div className="home">
         <h2>Homepage</h2>
         <button onClick={handleClick}>Click me</button>
         <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
+        <h1>State</h1>
+        <br />
+        <p>{ name } is { age } years old.</p>
+        <br />
+        <button onClick={handleClickName}>Click Me</button>
         </div>
      );
 }
